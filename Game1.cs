18328cf09b301f314d70edf4dce541dd0d739a60
Moses Ma <701000372@GoTVDSB.ca>
2026-01-14 11:15:18 -0500

    idek

--- Game1.cs
+++ Game1.cs
@@ -16,6 +16,7 @@ namespace spritesheet
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+<<<<<<< HEAD
         //Texture2D runbodySpritesheet;
         //Texture2D runheadSpritesheet;
         //Texture2D runshadowSpritesheet;
@@ -62,6 +63,32 @@ namespace spritesheet
         SpritesheetManager spritesheetManager;
 
         List<Texture2D> Idlespritesheets;
+=======
+        Texture2D runbodySpritesheet;
+        Texture2D runheadSpritesheet;
+        Texture2D runshadowSpritesheet;
+        Texture2D runswordSpritesheet;
+        Texture2D runswordbackSpritesheet;
+        Texture2D rectangleTexture;
+
+        RunBody playerBody;
+
+        //KeyboardState keyboardState;
+        //int rows, columns;
+        //int frame; // The frame number (column) in the sequence to draw
+        //int frames; // The number of frames for each direction, usually the same as columns
+        //int directionRow; // The row number containing the frames for the current direction
+        //int leftRow, rightRow, upRow, downRow; // Row number of directional set of frames
+        //int width; // The width of each frame
+        //int height; // The height of each frame
+        //float speed = 5f; // How fast the character sprite will travel
+        //float time; // Used to store elapsed time
+        //float frameSpeed; // Sets how fast player frames transition
+        //Vector2 playerLocation; // Stored the location of the players collision sprite
+        //Vector2 playerDirection; // The directional vector of the player
+        //Rectangle playerCollisionRect, playerDrawRect;
+
+>>>>>>> parent of f015987 (work on runattack class)
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -72,9 +99,22 @@ namespace spritesheet
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            //columns = 8;
+            //rows = 4;
+            //upRow = 0;
+            //leftRow = 1;
+            //rightRow = 2;
+            //downRow = 3;
             base.Initialize();
+            //directionRow = downRow; // Player will start facing down
+            //width = runbodySpritesheet.Width / columns;
+            //height = runbodySpritesheet.Height / rows;
+            //playerLocation = new Vector2(20, 20);
+            //playerCollisionRect = new Rectangle(80, 60, 40, 70);
+            //playerDrawRect = new Rectangle(20, 20, 150, 150);
+            //UpdatePlayerRects();
 
+<<<<<<< HEAD
             
 
             playerLocation = new Vector2(20, 20);
@@ -94,7 +134,14 @@ namespace spritesheet
             frame = 0;
 
             //playerBody = new RunBody(runbodySpritesheet, runheadSpritesheet, runswordSpritesheet, runswordbackSpritesheet, runshadowSpritesheet, rectangleTexture);
+=======
+            playerBody = new RunBody(runbodySpritesheet, runheadSpritesheet, runswordSpritesheet, runswordbackSpritesheet, runshadowSpritesheet);
+>>>>>>> parent of f015987 (work on runattack class)
 
+            //time = 0.0f;
+            //frameSpeed = 0.1f;
+            //frames = 8;
+            //frame = 0;
         }
 
         protected override void LoadContent()
@@ -102,6 +149,7 @@ namespace spritesheet
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+<<<<<<< HEAD
             List<Texture2D> Runningspritesheets = new()
             {
                 Content.Load<Texture2D>("Swordsman_lvl1_Run_body"),
@@ -137,6 +185,14 @@ namespace spritesheet
 
 
 
+=======
+            runbodySpritesheet = Content.Load<Texture2D>("Swordsman_lvl1_Run_body");
+            runheadSpritesheet = Content.Load<Texture2D>("Swordsman_lvl1_Run_head");
+            runshadowSpritesheet = Content.Load<Texture2D>("Swordsman_lvl1_Run_shadow");
+            runswordSpritesheet = Content.Load<Texture2D>("Swordsman_lvl1_Run_sword");
+            runswordbackSpritesheet = Content.Load<Texture2D>("Swordsman_lvl1_Run_sword_back");
+            rectangleTexture = Content.Load<Texture2D>("rectangle");
+>>>>>>> parent of f015987 (work on runattack class)
         }
 
         protected override void Update(GameTime gameTime)
@@ -146,10 +202,30 @@ namespace spritesheet
 
             // TODO: Add your update logic here
 
+<<<<<<< HEAD
             spritesheetManager.SetPlayerDirection(rectangle: rectangle, keyboardState: keyboardState, playerDirection: playerDirection, directionRow: directionRow, leftRow: leftRow, rightRow: rightRow, downRow: downRow, upRow: upRow, frame: frame);
             playerLocation += playerDirection * speed;
             UpdatePlayerRects();
             //playerBody.Update(gameTime);
+=======
+            
+
+            //time += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //if (time > frameSpeed && playerDirection != Vector2.Zero)
+            //{
+            //    time = 0f;
+            //    frame += 1;
+            //    if (frame >= frames)
+            //        frame = 0;
+            //}
+            //keyboardState = Keyboard.GetState();
+
+            //SetPlayerDirection();
+            //playerLocation += playerDirection * speed;
+            //UpdatePlayerRects();
+
+            playerBody.Update(gameTime);
+>>>>>>> parent of f015987 (work on runattack class)
 
             base.Update(gameTime);
         }
@@ -160,6 +236,7 @@ namespace spritesheet
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
+<<<<<<< HEAD
 
             //playerDrawRect = spritesheetManager.Rectangle;
             spritesheetDraw.Draw(_spriteBatch, new Rectangle(100, 100, 100, 100), directionRow);
@@ -182,5 +259,60 @@ namespace spritesheet
             playerDrawRect.X = playerCollisionRect.X - 55;
             playerDrawRect.Y = playerCollisionRect.Y - 40;
         }
+=======
+            playerBody.Draw(_spriteBatch);
+            //_spriteBatch.Draw(rectangleTexture, playerCollisionRect, Color.Black * 0.3f);
+            //_spriteBatch.Draw(runbodySpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+            //_spriteBatch.Draw(runheadSpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+            //_spriteBatch.Draw(runswordSpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+            //_spriteBatch.Draw(runswordbackSpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+            //_spriteBatch.Draw(runshadowSpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+        //public void UpdatePlayerRects()
+        //{
+        //    playerCollisionRect.Location = playerLocation.ToPoint();
+        //    playerDrawRect.X = playerCollisionRect.X - 55;
+        //    playerDrawRect.Y = playerCollisionRect.Y - 40;
+        //}
+        //private void SetPlayerDirection()
+        //{
+        //    playerDirection = Vector2.Zero;
+
+
+        //    if (keyboardState.IsKeyDown(Keys.A))
+        //        playerDirection.X += -1;
+
+        //    if (keyboardState.IsKeyDown(Keys.D))
+        //        playerDirection.X += 1;
+
+        //    if (keyboardState.IsKeyDown(Keys.W))
+        //        playerDirection.Y += -1;
+
+        //    if (keyboardState.IsKeyDown(Keys.S))
+        //        playerDirection.Y += 1;
+
+
+        //    if (playerDirection != Vector2.Zero)
+        //    {
+        //        playerDirection.Normalize();
+        //        if (playerDirection.X < 0) // Moving left
+        //            directionRow = leftRow;
+
+        //        else if (playerDirection.X > 0) // Moving right
+        //            directionRow = rightRow;
+
+        //        else if (playerDirection.Y < 0) // Moving up
+        //            directionRow = upRow;
+
+        //        else
+        //            directionRow = downRow;
+
+        //    }
+        //    else
+        //        frame = 0;
+        //}
+>>>>>>> parent of f015987 (work on runattack class)
     }
 }
--- RunBody.cs
+++ RunBody.cs
@@ -24,6 +24,7 @@ public class RunBody
 
     public RunBody(Texture2D runbodySpritesheet, Texture2D runheadSpritesheet, Texture2D runswordSpritesheet, Texture2D runswordbackSpritesheet, Texture2D runshadowSpritesheet, Texture2D rectangleTexture)
     {
+<<<<<<< HEAD
         this.runbodySpritesheet = runbodySpritesheet;
         this.runheadSpritesheet = runheadSpritesheet;
         this.runswordSpritesheet = runswordSpritesheet;
@@ -45,6 +46,116 @@ public class RunBody
             time = 0f;
             frame += 1;
             if (frame >= frames)
+=======
+        int rows, columns;
+        int frame; // The frame number (column) in the sequence to draw
+        int frames; // The number of frames for each direction, usually the same as columns
+        int directionRow; // The row number containing the frames for the current direction
+        int leftRow, rightRow, upRow, downRow; // Row number of directional set of frames
+        int width; // The width of each frame
+        int height; // The height of each frame
+
+        Rectangle playerCollisionRect, playerDrawRect;
+
+
+        KeyboardState keyboardState;
+        Vector2 playerDirection;
+        Vector2 playerLocation;
+
+
+        Texture2D runbodySpritesheet, runheadSpritesheet, runswordSpritesheet, runswordbackSpritesheet, runshadowSpritesheet;
+
+        float time; // Used to store elapsed time
+        float frameSpeed; // Sets how fast player frames transition
+        float speed = 5f;
+
+        public RunBody(Texture2D runbodySpritesheet, Texture2D runheadSpritesheet, Texture2D runswordSpritesheet, Texture2D runswordbackSpritesheet, Texture2D runshadowSpritesheet)
+        {
+            this.runbodySpritesheet = runbodySpritesheet;
+            this.runheadSpritesheet = runheadSpritesheet;
+            this.runswordSpritesheet = runswordSpritesheet;
+            this.runswordbackSpritesheet = runswordbackSpritesheet;
+            this.runshadowSpritesheet = runshadowSpritesheet;
+
+            playerLocation = new Vector2(20, 20);
+            playerCollisionRect = new Rectangle(80, 60, 40, 70);
+            playerDrawRect = new Rectangle(20, 20, 150, 150);
+            UpdatePlayerRects();
+            columns = 8;
+            rows = 4;
+            upRow = 0;
+            leftRow = 1;
+            rightRow = 2;
+            downRow = 3;
+            directionRow = downRow; // Player will start facing down
+            width = runbodySpritesheet.Width / columns;
+            height = runbodySpritesheet.Height / rows;
+            time = 0.0f;
+            frameSpeed = 0.1f;
+            frames = 8;
+            frame = 0;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            SetPlayerDirection();
+            playerLocation += playerDirection * speed;
+            UpdatePlayerRects();
+            time += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (time > frameSpeed && playerDirection != Vector2.Zero)
+            {
+                time = 0f;
+                frame += 1;
+                if (frame >= frames)
+                    frame = 0;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(runbodySpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+            spriteBatch.Draw(runheadSpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+            spriteBatch.Draw(runswordSpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+            spriteBatch.Draw(runswordbackSpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+            spriteBatch.Draw(runshadowSpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+        }
+        private void SetPlayerDirection()
+        {
+            keyboardState = Keyboard.GetState();
+            playerDirection = Vector2.Zero;
+
+
+            if (keyboardState.IsKeyDown(Keys.A))
+                playerDirection.X += -1;
+
+            if (keyboardState.IsKeyDown(Keys.D))
+                playerDirection.X += 1;
+
+            if (keyboardState.IsKeyDown(Keys.W))
+                playerDirection.Y += -1;
+
+            if (keyboardState.IsKeyDown(Keys.S))
+                playerDirection.Y += 1;
+
+
+            if (playerDirection != Vector2.Zero)
+            {
+                playerDirection.Normalize();
+                if (playerDirection.X < 0) // Moving left
+                    directionRow = leftRow;
+
+                else if (playerDirection.X > 0) // Moving right
+                    directionRow = rightRow;
+
+                else if (playerDirection.Y < 0) // Moving up
+                    directionRow = downRow;
+
+                else
+                    directionRow = upRow;
+
+            }
+            else
+>>>>>>> parent of f015987 (work on runattack class)
                 frame = 0;
         }
     }
