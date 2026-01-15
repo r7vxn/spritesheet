9aaa816a235914e45ab41be2e61914a3d3571ed2
moses <moses.enze@gmail.com>
2026-01-15 17:21:14 -0500

    work on the bug when u go forward

--- SpritesheetDraw.cs
+++ SpritesheetDraw.cs
@@ -6,50 +6,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace spritesheet
 {
     public class SpritesheetDraw
     {
-        //This class is used for loading in the spritesheet and drawing them
-
-        int rows, columns;
-        int frame; // The frame number (column) in the sequence to draw
-        int frames; // The number of frames for each direction, usually the same as columns
-        int directionRow; // The row number containing the frames for the current direction
-        //int leftRow, rightRow, upRow, downRow; // Row number of directional set of frames
-        int width; // The width of each frame
-        int height; // The height of each frame
-        Animation state;
         List<List<Texture2D>> spritesheets;
-        List<Texture2D> spritesheet;
-        
-        public SpritesheetDraw(List<List<Texture2D>>wholelist)
-        {
-            this.spritesheets = wholelist;
-            this.spritesheet = wholelist[(int)Animation.Idle];
 
-            columns = 12;
-            rows = 4;
-            //upRow = 3;
-            //leftRow = 1;
-            //rightRow = 2;
-            //downRow = 0;
-            //directionRow = downRow; // Player will start facing down
-            width = spritesheet[(int)Animation.Idle].Width / columns;
-            height = spritesheet[(int)Animation.Idle].Height / rows;
-            frames = 8;
+        public SpritesheetDraw(List<List<Texture2D>> wholelist)
+        {
+            spritesheets = wholelist;
         }
-        public void Draw(SpriteBatch spriteBatch, Rectangle rectangle, int directionRow, Animation state, int frame)
+
+        public void Draw(SpriteBatch spriteBatch, Rectangle rectangle, int directionRow, Animation state, int frame, int columns, int rows)
         {
-            this.frame = frame;
+            if ((int)state >= spritesheets.Count) return;
 
-            this.state = state;
-            this.directionRow = directionRow;
-            this.spritesheet = spritesheets[(int)state];
-            foreach (var item in spritesheet)
+            List<Texture2D> currentSpritesheet = spritesheets[(int)state];
+
+            for (int i = 0; i < currentSpritesheet.Count; i++)
             {
-                spriteBatch.Draw(item, rectangle, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+                Texture2D layer = currentSpritesheet[i];
+                int width = layer.Width / columns;
+                int height = layer.Height / rows;
 
+                Rectangle sourceRect = new Rectangle(frame * width, directionRow * height, width, height);
+                spriteBatch.Draw(layer, rectangle, sourceRect, Color.White);
             }
         }
     }
--- SpritesheetManager.cs
+++ SpritesheetManager.cs
@@ -13,101 +13,25 @@ namespace spritesheet
     {
         List<SpritesheetDraw> spritesheetDraw;
         Rectangle rectangle;
-        KeyboardState keyboardState;
-        Vector2 playerDirection;
-        int directionRow;
-        int leftRow, rightRow, downRow, upRow;
-        float frame;
-        float speed = 5f;
-        float time = 0.0f;
-        float frameSpeed = 0.1f;
-        int frames = 8;
-
 
         public Rectangle Rectangle
         {
-            get => rectangle; 
+            get => rectangle;
             set => rectangle = value;
         }
+
         public SpritesheetManager(List<List<Texture2D>> wholelist)
         {
             spritesheetDraw = new List<SpritesheetDraw>();
             spritesheetDraw.Add(new SpritesheetDraw(wholelist));
-
-        }
-        public void Draw(SpriteBatch spriteBatch, Animation state, int frame)
-        {
-            this.frame = frame;
-
-            foreach (var spritesheet in spritesheetDraw)
-            {
-                spritesheet.Draw(spriteBatch, rectangle, directionRow, state, frame);
-            }
-        }
-        public void SetPlayerDirection(Rectangle rectangle, KeyboardState keyboardState, Vector2 playerDirection, int directionRow, int leftRow, int rightRow, int downRow, int upRow, float frame)
-        {
-            
-            this.keyboardState = keyboardState;
-            this.playerDirection = playerDirection;
-            this.directionRow = directionRow;
-            this.leftRow = leftRow;
-            this.rightRow = rightRow;
-            this.downRow = downRow;
-            this.upRow = upRow;
-            this.frame = frame;
-            this.rectangle = rectangle;
-
-            this.playerDirection = new Vector2(20, 20);
-
-            keyboardState = Keyboard.GetState();
-            playerDirection = Vector2.Zero;
-
-
-            if (keyboardState.IsKeyDown(Keys.A))
-                playerDirection.X += -1;
-
-            if (keyboardState.IsKeyDown(Keys.D))
-                playerDirection.X += 1;
-
-            if (keyboardState.IsKeyDown(Keys.W))
-                playerDirection.Y += -1;
-
-            if (keyboardState.IsKeyDown(Keys.S))
-                playerDirection.Y += 1;
-
-
-            if (playerDirection != Vector2.Zero)
-            {
-                playerDirection = Vector2.Normalize(playerDirection);
-                if (playerDirection.X < 0) // Moving left
-                    directionRow = leftRow;
-
-                else if (playerDirection.X > 0) // Moving right
-                    directionRow = rightRow;
-
-                else if (playerDirection.Y < 0) // Moving up
-                    directionRow = upRow;
-                else
-                    directionRow = downRow;
-
-            }
-            else
-                frame = 0;
         }
 
-        public void Update (GameTime gameTime, int frame)
+        public void Draw(SpriteBatch spriteBatch, Animation state, int frame, Rectangle rectangle, int directionRow, int columns, int rows)
         {
-            this.frame = frame;
-            time += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (time > frameSpeed && playerDirection != Vector2.Zero)
+            foreach (var sheet in spritesheetDraw)
             {
-                time = 0f;
-                frame += 1;
-                if (frame >= frames)
-                    frame = 0;
+                sheet.Draw(spriteBatch, rectangle, directionRow, state, frame, columns, rows);
             }
-            keyboardState = Keyboard.GetState();
-
         }
     }
 }
